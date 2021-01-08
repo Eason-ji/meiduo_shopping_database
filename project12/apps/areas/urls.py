a904f81443c16b2b8d7a8345885508977e676185
eason <123@123.com>
2021-01-09 01:00:02 +0800

    project12项目实现登录功能

--- project12/apps/areas/urls.py
+++ project12/apps/areas/urls.py
@@ -3,5 +3,5 @@ from apps.areas import views
 
 urlpatterns = [
     path("areas/",views.Province.as_view()),
-    path("areas/<parent_id>/", views.SubAreas.as_view())
+    path("areas/<pk>/", views.SubAreas.as_view()),
 ]
--- project12/apps/users/urls.py
+++ project12/apps/users/urls.py
@@ -8,4 +8,9 @@ urlpatterns=[
     path("usernames/<uc:username>/count/",views.UserName.as_view()),
     path("mobiles/<mc:mobile>/count/",views.UserMobile.as_view()),
     path("register/",views.Register.as_view()),
+    path("login/",views.LoginView.as_view()),
+    path("logout/",views.Logout.as_view()),
+    path("info/",views.InfoView.as_view()),
+    path("addresses/create/",views.CreateArea.as_view())
+
 ]
